8f1ac7a1b2ec026a57dd9d8af5eb7d60f00b9d86
AnnScherer <ann.hardt@gmx.de>
2023-03-16 10:46:16 +0100

    pop up fenster

--- kalender/src/pages/Home.js
+++ kalender/src/pages/Home.js
@@ -1,13 +1,29 @@
 import Calendar from "react-calendar";
+import React, { useState } from "react";
 // import "react-calendar/dist/Calendar.css";
 
 const Home = () => {
+  const [value, onChange] = useState(new Date());
+  const [test, setTest] = useState(false);
+
+  const popUpOpen = () => {
+    setTest(true);
+  };
+  const popUpClose = () => {
+    setTest(false);
+  };
+
   return (
     <div>
       <h2>Kalender</h2>
       <div className="kalender">
-        <Calendar/>
+        <Calendar onClickDay={popUpOpen} onChange={onChange} value={value} />
       </div>
+      {test && (
+        <div className="day">
+          hi<button onClick={popUpClose}>X</button>
+        </div>
+      )}
     </div>
   );
 };
